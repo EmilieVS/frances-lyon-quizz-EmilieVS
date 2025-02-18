87489430867eae61da9fa4158394848f6950076c
Carjardying <carolinejardy.pro@gmail.com>
2025-02-18 11:38:53 +0100

    questions et réponses en dynamiques (js)

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 import {quizzitch} from './questions.js';
 
 // Récupérer les emplacements pour injecter la question et les options
-const question = document.getElementById('question');
-const options = document.getElementById('options');
+const askQuestion = document.getElementById('question-text');
+const answers = document.getElementById('options-text');
 
 // Récupérer la première question
 const firstQuestion = quizzitch.questions[0];
 
 // Injecter le texte de la question dans l'emplacement dédié
-question.innerText = firstQuestion.text;
+askQuestion.innerText = firstQuestion.text;
 
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
-firstQuestion.options.forEach((options) => {
+firstQuestion.options.forEach(options => {
   const optionButton = document.createElement('button');
   optionButton.innerText = options;
-  __________.classList.add('__________');
-  __________.appendChild(__________);
+  answers.classList.add('options');
+  answers.appendChild(optionButton);
 });
